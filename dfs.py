2a04ef4e9f130e1f3d68b8288fee567a3a78c57e
urvi213 <urvimaheshwari1234@gmail.com>
2025-06-08 09:04:11 +0100

    Update dfs.py

--- dfs.py
+++ dfs.py
@@ -32,8 +32,25 @@ class Graph():
             if not visited[n]:
                 self.recursive_dfs(n,visited)
 
+    def check_path(self,a,b):
+        path_exists = False
+        stack1 = stack.Stack(self.n)
+        visited = []
+        stack1.push(a)
+        while not stack1.is_empty():
+            node = stack1.pop()
+            visited.append(node)
+            for n in self.adjacency_lists[node]:
+                if n not in visited and n not in stack1.stack:
+                        stack1.push(n)
+                if n == b:
+                    #print("exists")
+                    path_exists = True
+        #return visited
+        return path_exists
 
-graph1 = Graph(10)
+
+graph1 = Graph(15)
 #print(graph1.adjacency_lists)
 graph1.create_edge(0,2)
 graph1.create_edge(0,3)
@@ -45,6 +62,10 @@ graph1.create_edge(8,5)
 graph1.create_edge(7,5)
 graph1.create_edge(6,3)
 graph1.create_edge(4,8)
+graph1.create_edge(11,12)
 
 print(graph1.dfs(0))
 print(graph1.dfs_start())
+print(graph1.check_path(0,8))
+print(graph1.check_path(0,11))
+print(graph1.check_path(3,4))
